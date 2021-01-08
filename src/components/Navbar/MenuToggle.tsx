75776099765b56a6794010c3f8a235d5a351c162
Lotus <DecliningLotus@users.noreply.github.com>
2021-01-08 19:08:22 +0300

    fix: use correct chakra icon imports

--- src/components/Navbar/MenuToggle.tsx
+++ src/components/Navbar/MenuToggle.tsx
@@ -1,10 +1,10 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Icon } from "@chakra-ui/react";
 import { FiMenu as MenuIcon, FiX as CloseIcon } from "react-icons/fi";
 
 export const MenuToggle = ({ toggle, isOpen }) => {
   return (
     <Box display={{ base: "block", md: "none" }} onClick={toggle}>
-      {isOpen ? <CloseIcon /> : <MenuIcon />}
+      {isOpen ? <Icon as={CloseIcon} /> : <Icon as={MenuIcon} />}
     </Box>
   );
 };
