0c9a7f69e9f9af8ee11947b9a739b02dc443a271
Lotus <DecliningLotus@users.noreply.github.com>
2021-01-11 22:51:33 +0300

    fix: apply lighthouse recommendations

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  siteUrl: "https://example.com",
+  siteUrl: "https://next-lotus-starter.vercel.app/",
   generateRobotsTxt: true,
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,10 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
       <Head>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
     </ChakraProvider>
